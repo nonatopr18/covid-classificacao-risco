f756e6d7112d4cd2d1fd7918014ea8c588d4d379
nonatopr18 <nonato_pr1@hotmail.com>
2026-09-24 10:22:11 -0300

    acessar o app com senha

--- app.py
+++ app.py
@@ -2,7 +2,41 @@ import os
 import joblib
 import pandas as pd
 import streamlit as st
+##################### Gerar Senhas
+import streamlit as st
 
+def verificar_senha():
+
+    if "autenticado" not in st.session_state:
+        st.session_state.autenticado = False
+
+    if st.session_state.autenticado:
+        return True
+
+    st.title("🔐 Acesso restrito")
+
+    senha = st.text_input(
+        "Digite a senha:",
+        type="password"
+    )
+
+    if st.button("Entrar"):
+        if senha == st.secrets["senha_app"]:
+            st.session_state.autenticado = True
+            st.rerun()
+        else:
+            st.error("Senha incorreta.")
+
+    return False
+
+
+if not verificar_senha():
+    st.stop()
+
+st.title("Modelo de Recomendação do Instituto Inteligência de Dados - IID")
+
+
+####################Fim do Código Gerar Senha
 
 # ============================================================
 # CONFIGURAÇÃO
